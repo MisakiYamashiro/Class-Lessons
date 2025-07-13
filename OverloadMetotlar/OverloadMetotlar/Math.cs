a81d404a5bb5e1466e288d16b58389df19f02711
Efe Palaz <palazefe8796@gmail.com>
2025-07-13 14:20:18 +0300

    OverloadMetotlar
    Overload Metotlar Dersi

---
+++ OverloadMetotlar/OverloadMetotlar/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMetotlar
+{
+    internal class Math
+    {
+
+        //Overload Metotlar
+        //İsimleri aynı ancak metot imzaları(aldıkları parametrelerin türü veya sayısı) farklı olan metotlara denir. 
+        public int Topla(int sayi1, int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            return toplam;
+        }
+        //public double DoubleTopla(double sayi1, double sayi2)
+        //{
+        //    double toplam = sayi1 + sayi2;
+        //    return toplam;
+        //}
+        public double Topla(double sayi1, double sayi2)
+        {
+            double toplam = sayi1 + sayi2;
+            return toplam;
+        }
+
+        public double Topla(string sayi1, string sayi2)
+        {
+            double toplam = Convert.ToDouble(sayi1) + Convert.ToDouble(sayi2);
+            return toplam;
+        }
+    }
+}
---
+++ OverloadMetotlar/OverloadMetotlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMetotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mat = new Math();
+            Console.WriteLine(mat.Topla(2,5));
+
+            //Console.WriteLine(mat.Topla(12.9,12.8));
+            //Tanımlanan metot sadece int parametre aldığı için bu işlem yapılamaz
+
+            //Console.WriteLine(mat.DoubleTopla(12.9,8.7));
+
+            Console.WriteLine(mat.Topla(12.9,12.8));
+            //Parametrelere en uygun hangisi ise o overload'ı kullanır.
+
+            Console.WriteLine(mat.Topla("5","8.3"));
+        }
+    }
+}
