5f9137e4ce91dbf5214cd0910aa25f6d0cae5cba
Efe Palaz <palazefe8796@gmail.com>
2025-07-13 14:19:30 +0300

    PropertyKavrami
    Property Kavramı Dersi

---
+++ PropertyKavrami/PropertyKavrami/Insan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyKavrami
+{
+    internal class Insan
+    {
+        public string isim;
+        private string soyisim;
+        
+        public void setSoyisim(string soyisim)
+        {
+            this.soyisim = soyisim;
+            //this = bu sınıfa ait olan demek
+        } 
+
+        public string getSoyisim()
+        {
+            return soyisim;
+        }
+
+        public string DogumYeri { get; set; }//Auto Implamented Property
+        public int Yas { get; set; }
+    }
+}
---
+++ PropertyKavrami/PropertyKavrami/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyKavrami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Insan ins = new Insan();
+            ins.isim = "Murtaza";
+            ins.setSoyisim("Şuayipoğlu");
+            ins.DogumYeri = "İzmir";
+            Console.WriteLine(ins.isim + " " + ins.getSoyisim() + " " + ins.DogumYeri);
+        }
+    }
+}
